e72829fb06904615fe022dd25bf7b8315897cebc
pteacher <subfear@mail.ru>
2022-02-03 15:19:11 +0600

    init commit

---
+++ src/main/java/kg/edu/alatoo/cs204isaev/Cs204isaevApplication.java
@@ -0,0 +1,21 @@
+package kg.edu.alatoo.cs204isaev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Cs204isaevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs204isaevApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String index() {
+		return "<h1>Hello Ruslan!</h1>";
+	}
+
+}
---
+++ src/test/java/kg/edu/alatoo/cs204isaev/Cs204isaevApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.edu.alatoo.cs204isaev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs204isaevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
