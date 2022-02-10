3ce51d68559d672988f9bd551dc5938cfcea5d16
pteacher <subfear@mail.ru>
2022-02-10 16:05:10 +0600

    added thymeleaf dependency and 1 about page html also controller

---
+++ src/main/java/kg/edu/alatoo/cs204isaev/controllers/MainPageController.java
@@ -0,0 +1,14 @@
+package kg.edu.alatoo.cs204isaev.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainPageController {
+
+    @GetMapping("/")
+    public String mainPage() {
+        return "about";
+    }
+}
